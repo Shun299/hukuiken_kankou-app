6c04f46e5c13d410df390768fe004a795040d519
Shun299 <23610088ss@stu.yamato-u.ac.jp>
2025-08-21 22:10:15 +0900

    update

--- public/js/result.js
+++ public/js/result.js
@@ -72,10 +72,10 @@ window.onload = async () => {
 function createcard(item) {
     const card = document.createElement('div');
     card.innerHTML = `
-    <img src="../picture/kari1_1280_853.jpg">
-    <img src="../picture/kari2_768_512.jpg">
-    <img src="../picture/kari3_650_433.jpg">
-    <img src="../picture/kari4_1299_867.jpg">
+    <img src="../picture/kari1_1280_853.jpg" class="card-image">
+    <img src="../picture/kari2_768_512.jpg" class="card-image">
+    <img src="../picture/kari3_650_433.jpg" class="card-image">
+    <img src="../picture/kari4_1299_867.jpg" class="card-image">
     <h3>${item.name}</h3>
     <p>${item.address}</p>
     <a href="https://www.google.com/search?q=${item.name}" target="_blank">Googleで詳しく調べる</a>
