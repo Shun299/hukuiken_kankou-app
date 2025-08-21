a6eea38b00a84abe960ab7e4a70280bd0dbf8115
Shun299 <23610088ss@stu.yamato-u.ac.jp>
2025-08-21 16:18:25 +0900

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,42 @@
 const submit_button = document.getElementById('submit');
 
+// 中分類と小分類の対応表
+const categoryMap = {
+    '歴史・文化': ['史跡', '神社・仏閣', '庭園', '博物館', '美術館', '記念・資料館', '歴史的まち並み、旧街道', '城'],
+    '自然（行祭事・イベント）': ['海岸', '動・植物園', '山岳', '河川', '高原', '湖沼', '公園', 'その他自然'],
+    '温泉・健康': ['温泉地'],
+    'ｽﾎﾟｰﾂ･ﾚｸﾘｴｰｼｮﾝ': ['海水浴場', 'スポーツ・レクリエーション施設', '釣り場', 'キャンプ場', 'スキー場', '水族館', 'レジャーランド・遊園地', 'テーマパークテーマパーク'],
+    '都市型観光 買物 食': ['産業観光', '地区・商店街', '食・グルメ', 'その他都市型観光－買物・食等－'],
+    'その他': ['他に分類されない観光地点']
+};
+
+// 選択された中分類を調べて対応する小分類の項目のみを表示する
+window.onload = () => {
+    const middleselect = document.getElementById('middle-class');
+    const minorselect = document.getElementById('minor-class');
+
+    // 選択された中分類の項目から対応した小分類の項目をmiddleANDminorに入れる
+    middleselect.addEventListener('change', ()=> {
+        const middlevalue = middleselect.value;
+        const middleANDminor = categoryMap[middlevalue] || [];
+
+        minorselect.innerHTML = '<option hidden>小分類を選択してください</option>';
+
+        // 対応する小分類を新しく追加
+        middleANDminor.forEach(item => {
+            const option = document.createElement('option');
+            option.textContent = item;
+            option.value = item;
+            minorselect.appendChild(option);
+        });
+    });
+};
+
+
+
+
+
+
 // 検索ボタンを押されたときの処理
 submit_button.addEventListener('click', (event) => {
 
@@ -9,10 +46,10 @@ submit_button.addEventListener('click', (event) => {
     const minor = document.getElementById('minor-class').value;
 
     // もし一つでも選択しなかったら、アラート（警告ウィンドウ）を表示する
-    if (major === '大分類を選択してください' || middle === '中分類を選択してください' || minor === '小分類を選択してください') {
-        alert('すべての項目を選択してください。');
-        return; 
-    }
+    // if (major === '大分類を選択してください' || middle === '中分類を選択してください' || minor === '小分類を選択してください') {
+    //     alert('すべての項目を選択してください。');
+    //     return; 
+    // }
 
     // 各プルダウンメニューの値を取得からURLを作成
     location.href = `/html/result.html?major=${major}&middle=${middle}&minor=${minor}`
